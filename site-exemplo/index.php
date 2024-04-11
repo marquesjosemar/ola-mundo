06251f103c74e22b966b40edb7655894c22e48e5
Josemar Marques <markesdf@gmail.com>
2024-04-11 18:41:04 -0300

    criei o arquivo inicial

---
+++ site-exemplo/index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>olá mundo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aut consequatur recusandae doloribus exercitationem? Atque architecto ipsam eius earum inventore fugit illo sequi non ipsa libero, sit minus optio ut.</p>
+    
+</body>
+</html>
